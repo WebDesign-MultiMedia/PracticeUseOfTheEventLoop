647f82293853b5e20d0021c5b1713f156c5c4c53
j.salas2016 <jsalas198016@gmail.com>
2024-06-10 16:37:17 -0400

    added HTML

---
+++ app.js
@@ -0,0 +1,22 @@
+let example = 1;
+// Declare a global counter variable.
+
+// Create a simple function that increments the variable, and then calls itself recursively.
+
+ try{  function exPlus(counter){
+        for(let i = 0; i < 3000; example++){
+            console.log(counter)
+        }
+        exPlus(example) }
+} 
+catch (Exception e){
+        FileSystem.out.printIn("Error past 15,000 range")
+} 
+
+
+
+
+
+
+// Surround the initial function call in a try/catch block.
+// Within the catch, log the error and the value of the counter variable.
