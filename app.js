678998c0022ea9177be08f4dba3f2a208d1848d7
j.salas2016 <jsalas198016@gmail.com>
2024-06-11 10:32:25 -0400

    Done with part One

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-let example = 1;
-// Declare a global counter variable.
+//Declare a global counter variable.
+let counter = 1;
 
-// Create a simple function that increments the variable, and then calls itself recursively.
+function increment(){
+    counter++;
+    increment()
+}
+try{
+    increment();
+}
+catch{
+    console.log("Maximum call stack size exceeded at: ", counter)
+}
 
- try{  function exPlus(counter){
-        for(let i = 0; i < 3000; example++){
-            console.log(counter)
-        }
-        exPlus(example) }
-} 
-catch (Exception e){
-        FileSystem.out.printIn("Error past 15,000 range")
-} 
-
-
-
-
-
-
-// Surround the initial function call in a try/catch block.
-// Within the catch, log the error and the value of the counter variable.
+/**Create a simple function that increments
+ *  the variable, and then calls 
+ * itself recursively.
+ */
